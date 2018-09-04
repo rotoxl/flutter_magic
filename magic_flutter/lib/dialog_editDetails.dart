6c70c297760c558cb3ec953be59731272541376e
ermolina <ermolina@e-externas.aena.es>
2018-09-04 07:47:31 +0200

    List & details pages added Details page configuration, saved in SharedPreferences

---
+++ magic_flutter/lib/dialog_editDetails.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:magic_flutter/model.dart';
+
+class FullScreenDialogDemo extends StatefulWidget {
+  final EndPoint endPoint;
+  FullScreenDialogDemo(this.endPoint, {Key key}) : super(key: key);
+
+  @override
+  FullScreenDialogDemoState createState() =>
+      new FullScreenDialogDemoState(endPoint);
+}
+
+class FullScreenDialogDemoState extends State<FullScreenDialogDemo> {
+  EndPoint endPoint;
+
+  String _fieldID, _fieldName, _fieldImage;
+  String _fieldMain, _fieldSecondary;
+
+  final TextEditingController _fieldNameController = new TextEditingController();
+  final TextEditingController _fieldMainController = new TextEditingController();
+  final TextEditingController _fieldSecondaryController = new TextEditingController();
+
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  FullScreenDialogDemoState(this.endPoint);
+
+  @override
+  void initState() {
+    _fieldName = this.endPoint.nameField;
+
+    _fieldID = this.endPoint.idField;
+    _fieldImage = this.endPoint.imgField;
+
+    _fieldMain = this.endPoint.mainFields.join(", ");
+    _fieldSecondary= this.endPoint.secondaryFields.join(", ");
+
+    _fieldNameController.text = _fieldName;
+    _fieldMainController.text = _fieldMain;
+    _fieldSecondaryController.text = _fieldSecondary;
+
+    return super.initState();
+  }
+
+  void _handleSubmitted() {
+    final FormState form = _formKey.currentState;
+    if (!form.validate()) {
+    } else {
+      form.save();
+
+      this.endPoint.idField=_fieldID;
+      this.endPoint.imgField=_fieldImage;
+      this.endPoint.nameField=_fieldName;
+
+      var mainTemp=_fieldMain.split(",").map( (s) => s.trim() );
+      var secTemp=_fieldSecondary.split(",").map( (s) => s.trim() );
+
+      this.endPoint.mainFields=mainTemp.toList();
+      this.endPoint.secondaryFields=secTemp.toList();
+
+      this.endPoint.save().then((result) {
+        print("Saving done: ${result}.");
+      });
+
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    //
+    var listItems = endPoint.fields().map((String value) {
+      return new DropdownMenuItem<String>(
+        value: value,
+        child: new Text(value),
+      );
+    }).toList();
+
+    return new Scaffold(
+      appBar:new AppBar(
+          title: new Text('Edit details page'),
+          actions: <Widget>[
+            new FlatButton(
+                child: new Text('SAVE'),
+                onPressed: () {
+                  _handleSubmitted();
+                })
+          ]
+        ),
+      body: new Form(
+          key: _formKey,
+//          autovalidate: _autovalidate,
+//          onWillPop: _warnUserAboutInvalidData,
+          child: new ListView(
+              padding: const EdgeInsets.all(16.0),
+              children: <Widget>[
+                new Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    alignment: Alignment.bottomLeft,
+                    child: new Row(
+                      children: <Widget>[
+                        new Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            new Text(
+                              'ID Field',
+                              style: theme.textTheme.caption,
+                              textAlign: TextAlign.start,
+                            ),
+                            new DropdownButton(
+                                items: listItems,
+                                value: _fieldID,
+                                onChanged: (String value) {
+                                  setState(() {
+                                    _fieldID = value;
+                                  });
+                                }),
+                          ],
+                        ),
+                        new Expanded(child:
+                        new Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: <Widget>[
+                            new Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                new Text(
+                                  'Image Field',
+                                  style: theme.textTheme.caption,
+                                  textAlign: TextAlign.start,
+                                ),
+                                new DropdownButton(
+                                    items: listItems,
+                                    value: _fieldImage,
+                                    onChanged: (String value) {
+                                      setState(() {
+                                        _fieldImage = value;
+                                      });
+                                    }),
+                              ],
+                            )
+                          ],
+                        )
+                        )
+                      ],
+                    )),
+                new Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    alignment: Alignment.bottomLeft,
+                    child: new TextField(
+                        decoration: const InputDecoration(labelText: 'Name field', hintText: 'field1',),
+                        autocorrect: false,
+                        controller: _fieldNameController,
+                        onChanged: (String value) {
+                          setState(() {
+                            _fieldName = value;
+                          });
+                        })),               new Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    alignment: Alignment.bottomLeft,
+                    child: new TextField(
+                        decoration: const InputDecoration(labelText: 'Main section fields (CSV)', hintText: 'field1, field2',),
+                        autocorrect: false,
+                        controller: _fieldMainController,
+                        onChanged: (String value) {
+                          setState(() {
+                            _fieldMain = value;
+                          });
+                        })),
+                new Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    alignment: Alignment.bottomLeft,
+                    child: new TextField(
+                        decoration: const InputDecoration(labelText: 'Secondary section fields (CSV)', hintText: 'field1, field2',),
+                        autocorrect: false,
+                        controller: _fieldSecondaryController,
+                        onChanged: (String value) {
+                          setState(() {
+                            _fieldSecondary = value;
+                          });
+                        })),
+              ].map((Widget child) {
+                return new Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    height: 96.0,
+                    child: child);
+              }).toList())),
+    );
+  }
+}
---
+++ magic_flutter/lib/widgets.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class WidgetCategory extends StatelessWidget {
+  const WidgetCategory({ Key key, this.icon, this.children }) : super(key: key);
+
+  final IconData icon;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    return new Container(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      decoration: new BoxDecoration(
+          border: new Border(bottom: new BorderSide(color: themeData.dividerColor))
+      ),
+      child: new DefaultTextStyle(
+        style: Theme.of(context).textTheme.subhead,
+        child: new SafeArea(
+          top: false,
+          bottom: false,
+          child: new Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Container(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  width: 72.0,
+                  child: new Icon(icon, color: themeData.primaryColor)
+              ),
+              new Expanded(child: new Column(children: children))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WidgetCategoryItem extends StatelessWidget {
+  WidgetCategoryItem({ Key key, this.icon, this.lines, this.tooltip, this.onPressed })
+      : assert(lines.length > 1),
+        super(key: key);
+
+  final IconData icon;
+  final List<String> lines;
+  final String tooltip;
+  final Function(BuildContext) onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    final List<Widget> columnChildren = lines.sublist(0, lines.length - 1).map((String line) => new Text(line)).toList();
+    columnChildren.add(new Text(lines.last, style: themeData.textTheme.caption));
+
+    final List<Widget> rowChildren = <Widget>[
+      new Expanded(
+          child: new Column(crossAxisAlignment: CrossAxisAlignment.start, children: columnChildren)
+      )
+    ];
+
+    rowChildren.add(new SizedBox(
+        width: 72.0,
+        child: icon!=null?
+        new IconButton(
+            icon: new Icon(icon),
+            color: themeData.primaryColor,
+            onPressed: (){onPressed(context);}
+        ): Text(" ")
+
+    ));
+
+    return new MergeSemantics(
+      child: new Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: rowChildren
+          )
+      ),
+    );
+  }
+}
