b7754b10eaa8ebda941bd82d1dd8872955b404aa
ermolina <ermolina@e-externas.aena.es>
2018-10-18 09:26:07 +0200

    build stuff

--- magic_flutter/app/ios/Runner/AppDelegate.m
+++ magic_flutter/app/ios/Runner/AppDelegate.m
@@ -1,13 +1,17 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+@import Firebase;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    // Override point for customization after application launch.
+    
+    [FIRApp configure];
+    
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 @end
